d29e9f16512f6f1d6ad791213ed0ec12631afc85
ag-af <agabialves11@gmail.com>
2024-11-07 17:51:04 +0000

    feat: Implement take_quiz method and randomise questions order.

--- quiz_game.py
+++ quiz_game.py
@@ -1,4 +1,6 @@
 import random
+from multiprocessing.managers import Value
+
 
 class Quiz:
     def __init__(self):
@@ -7,3 +9,25 @@ class Quiz:
 
         def add_question(self, question, answer):
             self.questions.append((question, answer))
+
+            def take_quiz(self, username):
+                score = 0
+
+                questions_asked = random.sample(self.questions, len(self.questions))
+                for i, (question, answer) in enumerate(questions_asked, 1):
+                    while True:
+                        try:
+                            user_answer = input(f"Q{i}: {question} ").strip()
+                            if not user_answer:
+                                raise ValueError("Answer cannot be empty. Please enter an answer.")
+
+                            if user_answer.lower() == answer.lower():
+                                score += 1
+                            else:
+                                print(f"Incorrect! The correct answer was: {answer}")
+                                break
+                        except ValueError as e:
+                            print(e)
+
+                            print(f"\n{username}, you scored {score} out of {len(self.questions)}")
+                            self.scores[username] = score
